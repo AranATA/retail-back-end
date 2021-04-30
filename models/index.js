20b79bbbfadfd93752fe85024e2552b8007414e8
Ghibli <aran.t.ali@gmail.com>
2021-04-30 01:59:18 -0400

    file structure completed; model files are ready.

---
+++ models/index.js
@@ -0,0 +1,24 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category (DONE)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+// Categories have many Products (DONE)
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+// Products belongToMany Tags (through ProductTag) (DONE)
+Product.belongsToMany(Tag, {through: 'ProductTag'});
+// Tags belongToMany Products (through ProductTag) (DONE)
+Tag.BelongsToMany(Product, {through: 'ProductTag'});
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
